8726ba1b37a94583eeb06a7f8b6ce83ad37ef4c3
AtharvATAR <atharvpriyansh@gmail.com>
2021-11-13 09:11:55 +0530

    Renaming File

--- 1. BoardProject/RhombusSimple.java
+++ 1. BoardProject/RhombusSimple.java
@@ -2,14 +2,17 @@ import java.util.*;
 
 class RhombusSimple {
     public static void main(String[] args) {
-        //
+        // making object of Scanner class
         Scanner input = new Scanner(System.in);
 
+        // calling variables
         int inputNum, loopVar, loopVar2, loopVar3;
 
+        // input
         System.out.print("Enter The Number Of Rows/Columns: ");
         inputNum = input.nextInt();
 
+        // looping statements
         for (loopVar = 1; loopVar <= inputNum; loopVar++) {
             for (loopVar2 = 1; loopVar2 <= inputNum - loopVar; loopVar2++) {
                 System.out.print(" ");
@@ -20,6 +23,7 @@ class RhombusSimple {
             System.out.println();
         }
 
+        // closing object
         input.close();
     }
 }
